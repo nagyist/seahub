cc5ea81a53f3c181f0d5ed9ea5d69f0fa3770205
Aries <urchinzhou@gmail.com>
2026-05-09 13:45:43 +0800

    Feat/dnd upload in table view (#8948)
    * feat: dirtableview support uploading files by dnd
    
    * fix: custom permission and ctime formatter
    
    ---------
    
    Co-authored-by: zhouwenxuan <aries@Mac.local>

--- frontend/src/metadata/components/cell-formatter/ctime.js
+++ frontend/src/metadata/components/cell-formatter/ctime.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import dayjs from 'dayjs';
 import { formatWithTimezone } from '../../../utils/time';
 
 const CTimeFormatter = ({ value, className, children: emptyFormatter, format }) => {
-  if (!value) return emptyFormatter || null;
+  const displayValue = useMemo(() => {
+    if (dayjs.isDayjs(value)) {
+      return value.valueOf();
+    }
+    if (typeof value === 'number') {
+      // Some sources provide Unix seconds, normalize them to milliseconds
+      return value < 1000000000000 ? value * 1000 : value;
+    }
+    return value;
+  }, [value]);
+
+  if (!displayValue) return emptyFormatter || null;
+
   return (
     <div
       className={classnames('sf-metadata-ui cell-formatter-container ctime-formatter', className)}
-      title={formatWithTimezone(value)}
+      title={formatWithTimezone(displayValue)}
     >
-      {format == 'relativeTime' ? dayjs(value).fromNow() : dayjs(value).format('YYYY-MM-DD HH:mm:ss')}
+      {format == 'relativeTime' ? dayjs(displayValue).fromNow() : dayjs(displayValue).format('YYYY-MM-DD HH:mm:ss')}
     </div>
   );
 };
 
 CTimeFormatter.propTypes = {
   format: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.any,
   className: PropTypes.string,
   children: PropTypes.any,
 };
